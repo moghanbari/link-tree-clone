e82c4ae0fb774f165da56e13d1eb581f521ae2ac
Mo Ghanbari <mo.ghanbari@icloud.com>
2022-07-24 14:56:40 -0400

    initial project & getting port from dotenv

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
